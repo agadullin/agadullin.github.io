c885bd7c66f7b0e95cbd0ba3c0183638d17f5538
agadullin <otp_a.agadullin@mail.ru>
2019-01-21 01:47:15 +0300

    mogo

---
+++ slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.work__wrap').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+});
+});
+
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [55.75394149, 37.62217172],
+        zoom: 14
+    });
+    }
+
