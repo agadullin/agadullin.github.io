ea2b5dddcde07dcada11f1c0ac9d77594a177fa1
agadullin <otp_a.agadullin@mail.ru>
2019-01-21 03:14:05 +0300

    add site

---
+++ second/menu.js
@@ -0,0 +1,6 @@
+const x = document.getElementById("myMenu");
+const y = document.getElementById("menu");
+
+y.onclick = function () {
+    x.classList.toggle("nav__click");
+};
---
+++ second/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.works__content').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 700
+    });
+});
